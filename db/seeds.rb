c17c9d27f409a430d9713e36098f607a89592e57
Thomas Maher <Thomasmaher1210@gmail.com>
2026-07-27 22:46:48 -0400

    Create docker-compose and set up data to run pgsql on docker.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+user = User.create(first_name: 'Tom', last_name: 'Maher')
+
+session1 = Session.create(date: Time.zone.today - 7.days, gym_name: 'Vital', user_id: user.id)
+session2 = Session.create(date: Time.zone.today - 2.days, gym_name: 'Vital', user_id: user.id)
+
+b1 = Boulder.create(vgrade_range_min: 6, vgrade_range_max: 7, self_grade: 4, incline: 0, rating: 3, boulder_type: 'Indoor', nickname: 'Yellow-spooky-slab')
+b2 = Boulder.create(vgrade_range_min: 2, vgrade_range_max: 3, self_grade: 4, incline: 60, rating: 4, boulder_type: 'Indoor', nickname: 'Flowy-Orange-outdoor-cave')
+
+SessionClimb.create(session_id: session1.id, user_id: user.id, boulder_id: b1.id, attempts: 3, percent_finished: 100, warmup: false)
+SessionClimb.create(session_id: session1.id, user_id: user.id, boulder_id: b2.id, attempts: 1, percent_finished: 100, warmup: true)
+SessionClimb.create(session_id: session2.id, user_id: user.id, boulder_id: b2.id, attempts: 1, percent_finished: 100, warmup: true)

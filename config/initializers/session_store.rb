df027e7ea20f6d902eee444992c02809e74e0787
Thomas Maher <Thomasmaher1210@gmail.com>
2026-08-13 15:18:16 -0400

    Docker compose updates

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_climb_journal_session"
+Rails.application.config.session_store :cookie_store, key: "_climb_journal_session",  expire_after: 2.hours
